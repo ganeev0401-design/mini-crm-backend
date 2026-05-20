0e94a09b42ee040d51e0971faf10e6c77de86bfe
Artem Ganeev <artemganeev@MacBook-Pro-od-Artem.local>
2026-05-20 17:06:52 +0500

    fix port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/users', async (req, res) => {
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log("Server running")
+  console.log("Server running on port " + PORT)
 })
 
 // endpoint на beckend
